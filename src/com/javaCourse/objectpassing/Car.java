272ef8690831b313a80313c69eb33daa7bf1fa13
Taha <mt.zngn@gmail>
2021-03-09 16:55:11 +0000

    object passing covered

---
+++ src/com/javaCourse/objectpassing/Car.java
@@ -0,0 +1,9 @@
+package com.javaCourse.objectpassing;
+
+public class Car {
+    String name;
+
+    Car(String name){
+        this.name = name;
+    }
+}
---
+++ src/com/javaCourse/objectpassing/Garage.java
@@ -0,0 +1,8 @@
+package com.javaCourse.objectpassing;
+
+public class Garage {
+
+    void park(Car car){
+        System.out.println("The " + car.name + " is parked in garage");
+    }
+}
